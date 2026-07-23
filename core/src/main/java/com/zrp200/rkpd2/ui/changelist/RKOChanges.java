ab10590c9f4488cfa4dd9a3182008dd8925340ab
jq4269 <201323747+jq4269@users.noreply.github.com>
2026-07-24 09:57:05 +1200

    v2.3.1: added line in RKOChanges explaining BowSpirit internal name

--- core/src/main/java/com/zrp200/rkpd2/ui/changelist/RKOChanges.java
+++ core/src/main/java/com/zrp200/rkpd2/ui/changelist/RKOChanges.java
@@ -29,7 +29,9 @@ public enum RKOChanges {
                                     list("Replaced Warlock with a new secret subclass for Huntress, _Channeler_!",
                                             "_Channeler_ can covert its Spirit Bow into a unique ally that attacks on its own, with new talents that support it in various ways.",
                                             "The spirit bow doesn't attack if the Channeler also attacked during the same turn for balancing reasons... for now.",
-                                            "I'm expecting this subclass to be a bit more difficult to play than Sniper and the other subclasses, so I don't mind buffing it more once I get some feedback on it.")
+                                            "I'm expecting this subclass to be a bit more difficult to play than Sniper and the other subclasses, so I don't mind buffing it more once I get some feedback on it.",
+                                            "The spawned spirit bow ally is internally called '_BowSpirit_', which is the name used in the Scroll Of Debug."
+                                        )
                             )
                     ),
                     Changes(
